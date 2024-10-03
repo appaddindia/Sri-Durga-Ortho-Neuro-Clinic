cfe9a571bacfb3d82526937e915bfe562f745114
appaddindia <appaddmaster@gmail.com>
2024-10-03 10:19:51 +0530

    first commit

--- src/components/team/team-details/team-details-sec.jsx
+++ src/components/team/team-details/team-details-sec.jsx
@@ -2,10 +2,8 @@ import DirectContactUs from "@/components/forms/direct-contact-us";
 import FooterFour from "@/layout/footer/footer-4";
 import HeaderTwo from "@/layout/header/header-two";
 import React from "react";
-import Banner from "./banner";
-import TeamDetailsArea from "./-area";
-import TeamDetailsAreaSec from "./-area-sec";
 import Bannersec from "./Bannersec";
+import TeamDetailsAreaSec from "./team-details-area-sec";
 
 const TeamDetailsSec = () => {
   return (
--- src/components/team/team-details/team-details.jsx
+++ src/components/team/team-details/team-details.jsx
@@ -3,7 +3,7 @@ import FooterFour from "@/layout/footer/footer-4";
 import HeaderTwo from "@/layout/header/header-two";
 import React from "react";
 import Banner from "./banner";
-import TeamDetailsArea from "./-area";
+import TeamDetailsArea from "./team-details-area";
 
 const TeamDetails = () => {
   return (
