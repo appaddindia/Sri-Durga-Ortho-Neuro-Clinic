5ed93100af1a757b29fbfae99e3ad3b44f011108
appaddindia <appaddmaster@gmail.com>
2024-12-11 11:17:42 +0530

    first commit

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -9,7 +9,7 @@ const SEO = ({ pageTitle }) => (
       </title>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="description" content="Sri Durga Ortho & Neuro Clinic is a premier healthcare facility specializing in orthopedic and neurological treatments. Our skilled team includes Dr. Shashikanth Vokkaleri, a Joint Replacement and Arthroscopic Surgeon, and Dr. Surbhi Chaturvedi, an award-winning Neurologist. We offer services like knee and hip replacements, arthroscopy, and neurological disorder management, prioritizing advanced care for your health and well-being. Book your appointment today!" />
-      <meta name="robots" content="noindex, follow" />
+      <meta name="robots" content="index, follow" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
