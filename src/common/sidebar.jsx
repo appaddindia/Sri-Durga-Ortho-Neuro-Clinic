c8e1a95afd6e087c7acf436df143d40cd9e6ac17
appaddindia <appaddmaster@gmail.com>
2025-09-26 15:47:24 +0530

    update :social media links

--- src/common/sidebar.jsx
+++ src/common/sidebar.jsx
@@ -1,8 +1,7 @@
-import MobileMenus from '@/layout/header/mobile-menus';
-import ImagePopup from '@/modals/ImagePopup';
-import Link from 'next/link';
+import MobileMenus from "@/layout/header/mobile-menus";
+import ImagePopup from "@/modals/ImagePopup";
+import Link from "next/link";
 import React, { useState } from "react";
-
 
 const images = [
   {
@@ -13,13 +12,10 @@ const images = [
   },
   {
     img: "/assets/img/blog/blog-in-03.jpg",
-  }
+  },
 ];
 
-
 const Sidebar = ({ isActive, setIsActive }) => {
-
- 
   // photoIndex
   const [photoIndex, setPhotoIndex] = useState(null);
   // image open state
@@ -32,7 +28,6 @@ const Sidebar = ({ isActive, setIsActive }) => {
   //  images
   const img = images.map((item) => item.img);
 
- 
   return (
     <>
       <div
@@ -60,17 +55,27 @@ const Sidebar = ({ isActive, setIsActive }) => {
 
         <div className="tpsideinfo__content mb-60">
           <p className=" d-none d-xl-block">
-          Sri Durga Ortho & Neuro Clinic offers expert orthopedic and neurological care. Led by Dr. Shashikanth Vokkaleri and Dr. Surbhi Chaturvedi, we provide advanced treatments. Book your appointment today!
+            Sri Durga Ortho & Neuro Clinic offers expert orthopedic and
+            neurological care. Led by Dr. Shashikanth Vokkaleri and Dr. Surbhi
+            Chaturvedi, we provide advanced treatments. Book your appointment
+            today!
           </p>
           <span>Contact Us</span>
-          <a style={{display:"flex"}} href="#">
-            <i style={{marginTop:"4px"}} className="fa-solid fa-star"></i>1st Floor, Shiva Ram Karanath Nagar, No. 42 & 25, 80 Feet Rd, MCEHS Layout, Rachenahalli,Hegde Nagar , Bengaluru, Karnataka 560077
+          <a style={{ display: "flex" }} href="#">
+            <i style={{ marginTop: "4px" }} className="fa-solid fa-star"></i>1st
+            Floor, Shiva Ram Karanath Nagar, No. 42 & 25, 80 Feet Rd, MCEHS
+            Layout, Rachenahalli,Hegde Nagar , Bengaluru, Karnataka 560077
           </a>
-          <a style={{display:"flex"}} href="tel:9980445660">
-            <i style={{marginTop:"4px"}} className="fa-solid fa-star"></i>9980445660
+          <a style={{ display: "flex" }} href="tel:9980445660">
+            <i style={{ marginTop: "4px" }} className="fa-solid fa-star"></i>
+            9980445660
           </a>
-          <a style={{display:"flex"}} href="mailto:sridurgaorthoneuroclinic@gmail.com">
-            <i style={{marginTop:"4px"}} className="fa-solid fa-star"></i>sridurgaorthoneuroclinic@gmail.com
+          <a
+            style={{ display: "flex" }}
+            href="mailto:sridurgaorthoneuroclinic@gmail.com"
+          >
+            <i style={{ marginTop: "4px" }} className="fa-solid fa-star"></i>
+            sridurgaorthoneuroclinic@gmail.com
           </a>
         </div>
         <div className="tpsideinfo__content-inputarea mb-60 d-none d-xl-block">
@@ -90,7 +95,6 @@ const Sidebar = ({ isActive, setIsActive }) => {
             {images.map((item, i) => (
               <a
                 key={i}
-               
                 style={{ cursor: "pointer" }}
                 onClick={() => handleImagePopup(i)}
                 className="popup-image"
@@ -101,17 +105,17 @@ const Sidebar = ({ isActive, setIsActive }) => {
           </div>
         </div>
         <div className="tpsideinfo__socialicon">
-          <a href="#">
+          <a href="https://www.youtube.com/@sridurgaorthoneuroclinic/">
             <i className="fa-brands fa-youtube"></i>
           </a>
-          <a href="#">
-            <i className="fa-brands fa-twitter"></i>
+          <a href="https://share.google/A4V80uN5VjBv7GHGx">
+            <i className="fa-brands fa-google"></i>
           </a>
-          <a href="#">
+          <a href="https://www.facebook.com/sridurgaorthoandneuro/">
             <i className="fa-brands fa-facebook-f"></i>
           </a>
-          <a href="#">
-            <i className="fa-brands fa-skype"></i>
+          <a href="https://www.instagram.com/sridurgaorthoneuroclinic/">
+            <i className="fa-brands fa-instagram"></i>
           </a>
         </div>
       </div>
